39adf50d77b3e086bcc36ad8bab9c0182ce0650c
Dime <dime.jordanov@gmail.com>
2021-09-13 06:28:29 +0200

    posleden updejt

---
+++ Controllers/AppController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KamelijaWeb.Controllers
+{
+    public class AppController: Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Контактирајте не";
+
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewBag.Title = "За Нас";
+            return View();
+        }
+    }
+}
