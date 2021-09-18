56fbe7ec027658d07bb6dccad92ade23ee9fc815
Dime <dime.jordanov@gmail.com>
2021-09-18 19:00:48 +0200

    Pred Configuracija

---
+++ Data/KamContext.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using KamelijaWeb.Data.Entities;
+
+namespace KamelijaWeb.Data
+{
+    public class KamContext:DbContext
+    {
+        public DbSet<Product> Products{ get; set; }
+        public DbSet<Order> Orders { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer();
+        }
+            
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
+using KamelijaWeb.Data;
 using KamelijaWeb.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -17,6 +19,8 @@ namespace KamelijaWeb
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<KamContext>();
+
             services.AddTransient<IMailService, NullMailService>();
             services.AddControllersWithViews()
                 .AddRazorRuntimeCompilation();
