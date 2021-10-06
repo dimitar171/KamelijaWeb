ed41c348ff4fe479a21502a9392414339d0277a8
Dime <dime.jordanov@gmail.com>
2021-10-06 02:34:37 +0200

    angular raboti

---
+++ client/src/app/views/productListView.component.ts
@@ -0,0 +1,15 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector:"product-list",
+    templateUrl:"productListView.component.html"
+})
+export default class ProductListView {
+    public products = [{
+        title: "Van gogh mugh",
+        price: "100"
+    }, {
+        title: "Van gogh poster",
+        price: "200"
+    }];
+}
